e46abfc05ac37e36f64221a5a7241b25b2f57029
Arslonbek <79700403+ArslonbekXushboqov@users.noreply.github.com>
2022-01-04 18:46:59 +0500

    Update buttons.py

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,1 +1,10 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+til = InlineKeyboardMarkup(row_with=3)
+til_key = [        
+    InlineKeyboardButton('🇺🇿 UZ', callback_data='lang_uz'),
+    InlineKeyboardButton('🇷🇺 RU', callback_data='lang_ru'),
+    InlineKeyboardButton('🇺🇸 EN', callback_data='lang_en')
+    ]
+til.add(*til_key)
